090cdbdc0e62051000f5aa0b3d2828e17a32554f
BJT User <bjoern.foersterling@web.de>
2022-03-08 09:37:14 +0000

    Upload New File

---
+++ twofiles/Secondfile.java
@@ -0,0 +1,8 @@
+public class Secondfile {
+    //trying to call this class from another class/file
+
+    public static void report() {
+        System.out.println("this is the second file");
+    }
+
+}
